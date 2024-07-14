3c86f5679c9943ec5cf1b001ae81c8c8e16872ef
Pedro Camargo <sistemamagno@gmail.com>
2024-07-13 19:58:25 -0500

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  endOfLine: "auto",
+  jsxSingleQuote: true,
+  printWidth: 140,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false
+}
